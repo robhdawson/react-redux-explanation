d0bbc3d78a32d79e21232d2ee8e756743c31b10c
Rob Dawson <robhdawson@gmail.com>
2017-02-23 23:02:01 -0500

    indentation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="main">
-        <h1 className="title">hey</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="main">
+                <h1 className="title">hey</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
