d0f41d69aeec5f03a020fab51a0d9e81f05ec084
Rob Dawson <robhdawson@gmail.com>
2017-02-23 23:55:30 -0500

    add all them logs

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,54 +1,58 @@
 import { combineReducers } from 'redux';
 
+function log(name, state, action) {
+    console.log(`reducer '${name}' processing action '${JSON.stringify(action)}'`);
+    console.log(`current state: ${state}`);
+}
+
 const todd = (state = 'fine', action) => {
+    log('todd', state, action);
     switch(action.type) {
         case 'TODD':
+            console.log(`modifying state to: ${action.mood}`);
+            console.log('');
             return action.mood;
         default:
+            console.log('keeping current state');
+            console.log('');
             return state;
     }
 }
 
 const angela = (state = 'okay', action) => {
+    log('angela', state, action);
     switch(action.type) {
         case 'ANGELA':
+            console.log(`modifying state to: ${action.mood}`);
+            console.log('');
             return action.mood;
         default:
-            return state;
-    }
-}
-
-const ricardo = (state = 'content', action) => {
-    switch(action.type) {
-        case 'RICARDO':
-            return action.mood;
-        default:
-            return state;
-    }
-}
-
-const spurgeon = (state = 'hunky dory', action) => {
-    switch(action.type) {
-        case 'SPURGEON':
-            return action.mood;
-        default:
+            console.log('keeping current state');
+            console.log('');
             return state;
     }
 }
 
 const maryanne = (state = 'medium', action) => {
+    log('maryanne', state, action);
     switch(action.type) {
         case 'MARYANNE':
+            console.log(`modifying state to: ${action.mood}`);
+            console.log('');
             return action.mood;
         default:
+            console.log('keeping current state');
+            console.log('');
+            console.log('');
+            console.log('----');
+            console.log('');
+            console.log('');
             return state;
     }
 }
 
 export default combineReducers({
     todd,
-    ricardo,
-    spurgeon,
     angela,
     maryanne,
 });
